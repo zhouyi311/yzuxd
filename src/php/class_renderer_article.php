952827c74ceea078e8ac1e5f748f8f15315cce88
Yi Zhou <zhouyi311@hotmail.com>
2023-08-08 23:02:38 -0700

    add class

--- src/php/class_renderer_article.php
+++ src/php/class_renderer_article.php
@@ -35,19 +35,43 @@ class ArticleContentRenderer {
         // logic for rendering the section
         $projPath = $this->projPath;
         $headline = $section['headline'];
+        $headlineId = $section['headlineId'];
         $subhead = isset($section['subhead']) ? htmlspecialchars($section['subhead']) : null;
         $subheadCaption = isset($section['subheadCaption']) ? htmlspecialchars($section['subheadCaption']) : null;
         $subheadList = isset($section['subheadList']) ? ($section['subheadList']) : null;
 
+        echo "<section class='page_section article_section' id='{$headlineId}'>";
+        echo "<div class='container'><div class='row'>";
         
+        echo "<div class='section_headline col-lg-4 pe-lg-5'>";
+        if (isset($subhead)) {
+            echo "<h6 class='text-secondary'>$subhead</h6>";
+        }
+        echo "<h2 id='{$headlineId}' class='mb-3'>{$headline}</h2>";
+        if (isset($subheadCaption)) {
+            echo "<p class='text-body-tertiary'>$subheadCaption</p>";
+        }
+        if (isset($subheadList)) {
+            echo "<ul class='subhead_list'>";
+            foreach ($subheadList as $i => $item) {
+                $item = htmlspecialchars($item);
+                echo $i === 0 ? "<li class='fw-bold'> $item</li>" : "<li class=''>$item</li>";
+            }
+            echo "</ul>";
+        }
+        echo "</div>";
 
         // Depending on the content type, call the respective method
+        echo "<div class='col-lg-8'>";
         foreach ($section['content'] as $index => $contentItem) {
             $type = $contentItem['type'];
             if (method_exists($this, "render{$type}")) {
                 $this->{"render{$type}"}($contentItem['content']);
             }
         }
+        echo "</div>";
+        echo "</div></div>";
+        echo "</section>";
     }
 
     private function renderText($content) {
